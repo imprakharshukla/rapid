8a281ce04c92c468e249430bfd7f2033dce19380
Prakhar Shukla <imprakharshukla@gmail.com>
2024-04-24 07:06:19 +0530

    update

--- apps/demo/next.config.mjs
+++ apps/demo/next.config.mjs
@@ -1,13 +1,21 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
-
+import WebpackBar from "webpackbar";
 /** @type {import("next").NextConfig} */
 const config = {
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.plugins = [...config.plugins, new PrismaPlugin()];
     }
+     config.plugins.push(
+       new WebpackBar({
+         fancy: true,
+         profile: true,
+         basic: false,
+       })
+     );
+
     return config;
   },
   /** Enables hot reloading for local packages without a build step */
